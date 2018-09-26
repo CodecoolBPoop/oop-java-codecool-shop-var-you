556bb776ac16bb65c3c0962acecf9a81bbcfe73e
dewsy <dewsyy@gmail.com>
2018-09-26 14:54:33 +0200

    started decoding json

--- src/main/webapp/static/scripts.js
+++ src/main/webapp/static/scripts.js
@@ -1,5 +1,9 @@
 $(".cart-button").click(function () {
     $.get("/cart-request", function (data) {
-        console.log(data)
+        var products = JSON.parse(data);
+        for (var i = 0; i < products.length; i++) {
+            JSON.parse(i);
+            console.log(products);
+        }
     })
 });
