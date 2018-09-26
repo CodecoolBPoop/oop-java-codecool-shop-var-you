df97f241c6a2fe91a7553888cec997ab364c3df9
dewsy <dewsyy@gmail.com>
2018-09-26 14:22:21 +0200

    json arrives to javascript

--- src/main/java/com/codecool/shop/controller/CartRequest.java
+++ src/main/java/com/codecool/shop/controller/CartRequest.java
@@ -8,6 +8,8 @@ import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,23 +29,34 @@ import java.util.Map;
 public class CartRequest extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        WebContext context = new WebContext(req, resp, req.getServletContext());
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
 
-        /* DEmo cart setup */
+        /* Demo cart setup */
         List<Product> cartContent = new ArrayList<>();
         ProductCategory demoCategory = new ProductCategory("demoCategory", "hardvare", "just for demo");
         Supplier demoSupplier = new Supplier("demosuplier", "Just for demo");
-        Product demo1 = new Product("demo1", 300, "dollar", "Just for demo", demoCategory, demoSupplier);
-        Product demo2 = new Product("demo2", 300, "dollar", "Just for demo", demoCategory, demoSupplier);
-        Product demo3 = new Product("demo3", 300, "dollar", "Just for demo", demoCategory, demoSupplier);
-        Product demo4 = new Product("demo4", 300, "dollar", "Just for demo", demoCategory, demoSupplier);
+        Product demo1 = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", demoCategory, demoSupplier);
+        Product demo2 = new Product("Amazon Earth", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", demoCategory, demoSupplier);
+        Product demo3 = new Product("Amazon Water", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", demoCategory, demoSupplier);
+        Product demo4 = new Product("Amazon Air", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", demoCategory, demoSupplier);
         cartContent.add(demo1);
         cartContent.add(demo2);
         cartContent.add(demo3);
         cartContent.add(demo4);
 
-        context.setVariable("cartContent", cartContent);
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+
+        //create Json Object
+        JSONArray json = new JSONArray();
+
+        for(Product product : cartContent) {
+            json.add(product.toString());
+        }
+
+        // finally output the json string
+        out.println(json.toString());
 
     }
 
---
+++ src/main/webapp/static/scripts.js
@@ -0,0 +1,5 @@
+$(".cart-button").click(function () {
+    $.get("/cart-request", function (data) {
+        console.log(data)
+    })
+});
