e48d2a34c23e467902acfa662298bee2e7e81368
tillabakos <bakos.at@gmail.com>
2018-09-28 06:32:47 +0200

    Table rows added for shopping cart items for better formatting

--- src/main/webapp/static/scripts.js
+++ src/main/webapp/static/scripts.js
@@ -3,12 +3,12 @@ $(".cart-button").click(function () {
         var cartItems = $.parseJSON(data);
         var stringBuilder = [];
         for (var i = 0; i < cartItems.length; i++) {
-            stringBuilder.push('<div class="row"> <div class="col ml-auto">', cartItems[i].name, '</div>');
-            stringBuilder.push('<div class="ml-auto">', cartItems[i].price, '</div>');
-            stringBuilder.push('<div class="ml-auto">' +
-                ' <form method="post" action="/"><button class="btn btn-success add_to_cart" type="submit" name="add" value=',cartItems[i].id, '>+</button></form></div>');'\n'
-            stringBuilder.push('<div class="ml-auto">' +
-                ' <form method="post" action="/"><button class="btn btn-success add_to_cart" type="submit" name="remove" value=',cartItems[i].id, '>-</button></form></div>', '</div>');
+            stringBuilder.push('<tr> <td>', cartItems[i].name, '</td>');
+            stringBuilder.push('<td>', cartItems[i].price, '</td>');
+            stringBuilder.push('<td>' +
+                ' <form method="post" action="/"><button class="btn btn-success add_to_cart" type="submit" name="add" value=',cartItems[i].id, '>+</button></form></td>');'\n'
+            stringBuilder.push('<td>' +
+                ' <form method="post" action="/"><button class="btn btn-success add_to_cart" type="submit" name="remove" value=',cartItems[i].id, '>-</button></form></td> </tr>');
             $(".cart-table").empty();
             $(".cart-table").append(stringBuilder.join(""));
 
