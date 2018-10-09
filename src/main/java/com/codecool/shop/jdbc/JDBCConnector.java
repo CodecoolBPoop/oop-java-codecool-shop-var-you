ea8c9b87645d8f67489eb32ef8dcf143c0195812
tillabakos <bakos.at@gmail.com>
2018-10-09 10:50:51 +0200

    _

---
+++ src/main/java/com/codecool/shop/jdbc/JDBCConnector.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.jdbc;
+
+import java.sql.*;
+
+public class JDBCConnector {
+  private static final String DATABASE = "jdbc:postgresql://localhost:5432/todolist";
+  private static final String DB_USER = "tilla";
+  private static final String DB_PASSWORD = "xxx";
+
+}
